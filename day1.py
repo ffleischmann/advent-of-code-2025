61317d2e982e6719081c8f3131d0657722eb642f
Frank Fleischmann <frank.fleischmann@f24.com>
2025-12-01 10:20:23 +0100

    Day 1

---
+++ day1.py
@@ -0,0 +1,18 @@
+with open('day1_input.txt', 'r') as f:
+    instructions = [line.strip() for line in f]
+
+dial_value = 50
+zero_count = 0
+
+for instruction in instructions:
+    operation = instruction[0]
+    number = int(instruction[1:])
+    if operation == 'R':
+        dial_value = (dial_value + number) % 100
+    elif operation == 'L':
+        dial_value = (dial_value - number) % 100
+
+    if dial_value == 0:
+        zero_count += 1
+
+print("Total times dial hit zero:", zero_count)
